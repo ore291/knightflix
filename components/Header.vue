f7ac9ad7bc7c7c080f9d07d5f251a7154ffa442a
ore291 <oreoluwapadonu@gmail.com>
2021-04-28 15:29:30 +0100

    initial commit

---
+++ components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="">
+    this is the header
+    <nuxt-img
+      className="object-contain"
+      src="https://links.papareact.com/ua6"
+      width="200"
+      heigth="100"
+    />
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Header />
+</template>
+
+<script>
+
+export default {
+ 
+}
+</script>
+
+<style>
+
+</style>
